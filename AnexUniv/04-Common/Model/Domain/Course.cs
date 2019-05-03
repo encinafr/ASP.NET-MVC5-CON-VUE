8c316a2c20eb2f9004013c7864bbffa083970459
Javier Encina <encinajavier7@gmail.com>
2019-05-03 00:38:03 -0300

    Add base project

---
+++ AnexUniv/04-Common/Model/Domain/Course.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string Slug { get; set; }
+        public string Icon { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Image1 { get; set; }
+
+        public string Image2 { get; set; }
+
+        public decimal Vote { get; set; }
+
+        public Enums.Status Status { get; set; }
+
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
